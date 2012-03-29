ac96360f140694292c981cd7cdc928cec3546335
Ubuntu <olli.jarva@futurice.com>
2012-03-29 09:46:43 +0000

    json responses and little bit of documentation

--- upload.php
+++ upload.php
@@ -1,26 +1,36 @@
 <?
+// This file handles uploads from RT server and network monitoring server
+
 // File contains $password = "secret password for upload authentication";
 require_once("upload_settings.php");
+
+/* Only files in this array are allowed. Make sure web server user
+(typically apache, httpd or www-data) is allowed to write to these files */
+$what_allowed = array("ittickets.json", "netmap.png");
+
+function response($success, $status) {
+ return json_encode(array("success" => $success, "status" => $status));
+}
+
 if ($_POST["password"] != $password) {
- echo "Wrong password.";
+ echo response(false, "Wrong password");
  exit(0);
 }
-$what_allowed = array("ittickets.json", "netmap.png");
+
 if (!in_array($_POST["what"], $what_allowed)) {
- echo "Invalid target.";
+ echo response(false, "Invalid target name");
  exit(0);
 }
 
 $filename = "upload/".$_POST["what"]; // Already validated
 if (move_uploaded_file($_FILES["data"]["tmp_name"], $filename)) {
- echo "Upload succeeded";
+ echo response(true, "Upload succeeded");
  $ext = pathinfo($filename, PATHINFO_EXTENSION);
  if ($ext == "png") {
   exec("advpng -z1 \"$filename\"");
  }
 } else {
- echo "File upload failed: $filename\n";
- var_dump($_FILES);
+ echo response(false, "File upload failed: $filename\n");
 }
 
 ?>
