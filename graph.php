cbe492cd0d4d2b8293169b304140178371ccaa16
Ubuntu <olli.jarva@futurice.com>
2012-04-05 22:13:12 +0000

    Different colors according to percentage

--- graph.php
+++ graph.php
@@ -13,5 +13,12 @@ if (isset($_GET["title"])) {
  $extra = '-t "'.$title.'"';
 }
 
-system('rrdtool graph - --end now --start end-7d -u 100 -l 0 -r -v "Percentage" -c "SHADEA#FFFFFF" -c "SHADEB#FFFFFF" -c "FRAME#FFFFFF" -c "BACK#FFFFFF" '.$extra.' --width 400 "DEF:value1=/home/ubuntu/graphs/'.$filename.'.rrd:value:AVERAGE" "AREA:value1#00C000"');
+system('rrdtool graph - --end now --start end-7d -u 100 -l 0 -r -v "Percentage" -c "SHADEA#FFFFFF" -c "SHADEB#FFFFFF" -c "FRAME#FFFFFF" -c "BACK#FFFFFF" '.$extra.' --width 400'.
+       ' "DEF:value1=/home/ubuntu/graphs/'.$filename.'.rrd:value:AVERAGE" '.
+       ' "CDEF:value1green=value1,50,100,LIMIT" '.
+       ' "CDEF:value1yellow=value1,20,50,LIMIT" '.
+       ' "CDEF:value1red=value1,0,20,LIMIT" '.
+       ' "AREA:value1red#FF1300"' .
+       ' "AREA:value1yellow#DED00F"' .
+       ' "AREA:value1green#29FF00"');
 ?>
