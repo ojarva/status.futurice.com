85af5f946e5dfb40431ff7c3d45bc6206e896b95
Ubuntu <olli.jarva@futurice.com>
2012-04-15 15:13:04 +0000

    Indentation & ob_get_clean

--- graph/sauna.php
+++ graph/sauna.php
@@ -36,13 +36,13 @@ $modes = array(
 $modestring = $modes[$mode];
 
 if (isset($_GET["width"])) {
-   $width = min(2000, max(100, intval($_GET["width"])));
+    $width = min(2000, max(100, intval($_GET["width"])));
 }
 if (isset($_GET["height"])) {
-   $height = min(2000, max(100, intval($_GET["height"])));
+    $height = min(2000, max(100, intval($_GET["height"])));
 }
 if (isset($_GET["range"])) {
-   $range = min(192, max(1, intval($_GET["range"])));
+    $range = min(192, max(1, intval($_GET["range"])));
 }
 if (isset($_GET["mode"])) {
     $mode = $_GET["mode"];
@@ -55,22 +55,19 @@ http_cache_etag();
 $cachekey = "cache:sauna.png:$width:$height:$range:$mode";
 $data = $redis->get($cachekey);
 if ($data) {
- http_send_data($data);
+    http_send_data($data);
 } else {
- ob_start();
- system('rrdtool graph - --end now --start end-'.$range.'h --slope-mode -r '.
-     '--font TITLE:16:Helvetica --font WATERMARK:3:Helvetica --font AXIS:9:Helvetica --font UNIT:10:Helvetica '.
-     '-c "GRID#FFFFFF" -c "MGRID#FFFFFF" -c "ARROW#000000" -c "SHADEA#FFFFFF" -c "SHADEB#FFFFFF" -c "FRAME#FFFFFF" -c "BACK#FFFFFF" '.
-     '--full-size-mode --width '.$width.' --height '.$height.' '.
-     '"DEF:temperatureraw='.$filename.':temperature:AVERAGE" '.
-     '"CDEF:temperature=temperatureraw,24,110,LIMIT" '.
-     $modestring);
+    ob_start();
+    system('rrdtool graph - --end now --start end-'.$range.'h --slope-mode -r '.
+       '--font TITLE:16:Helvetica --font WATERMARK:3:Helvetica --font AXIS:9:Helvetica --font UNIT:10:Helvetica '.
+       '-c "GRID#FFFFFF" -c "MGRID#FFFFFF" -c "ARROW#000000" -c "SHADEA#FFFFFF" -c "SHADEB#FFFFFF" -c "FRAME#FFFFFF" -c "BACK#FFFFFF" '.
+       '--full-size-mode --width '.$width.' --height '.$height.' '.
+       '"DEF:temperatureraw='.$filename.':temperature:AVERAGE" '.
+       '"CDEF:temperature=temperatureraw,24,110,LIMIT" '.
+       $modestring);
 
-
-
- $data = ob_get_contents();
- ob_end_clean();
- $redis->setex($cachekey, 60, $data);
- http_send_data($data);
+    $data = ob_get_clean();
+    $redis->setex($cachekey, 60, $data);
+    http_send_data($data);
 }
 ?>
