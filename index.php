e3225ef52a2fba5c31081041b42eb58d93be7d3a
Ubuntu <olli.jarva@futurice.com>
2012-04-19 10:54:17 +0000

    Better output buffer callback for minifying page html

--- index.php
+++ index.php
@@ -35,10 +35,17 @@ foreach ($pages as $k => $v) {
     }
 }
 function callback($buffer) {
-    $buffer = str_replace("\r\n", " ", $buffer);
-    $buffer = str_replace("\n", " ", $buffer);
-    $buffer = str_replace("\t", " ", $buffer);
-    $buffer = preg_replace( '/\s+/', ' ', $buffer);
+    $search = array(
+        '/\>[^\S ]+/s', //strip whitespaces after tags, except space
+        '/[^\S ]+\</s', //strip whitespaces before tags, except space
+        '/(\s)+/s'  // shorten multiple whitespace sequences
+        );
+    $replace = array(
+        '>',
+        '<',
+        '\\1'
+        );
+    $buffer = preg_replace($search, $replace, $buffer);
     return $buffer;
 }
 ob_start("callback");
