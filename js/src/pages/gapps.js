0ed0edb520cfc30fc49b7fca6b0dcf75892a2b1e
Oskar Ehnström <oskar@ehnstrom.net>
2013-06-03 15:37:45 +0300

    Front-end support for google apps

---
+++ js/src/pages/gapps.js
@@ -0,0 +1,37 @@
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+};
+
+function fetch_data() {
+
+    var data = $("body").data("pagerefresh-data").content;
+    var today = data["gapps"]["today"];
+
+
+    $("#users").html(today["num_accounts"]);
+    $("#usage").html(bytesToSize(parseInt(today["usage_in_bytes"])));
+    $("#average").html(bytesToSize(parseInt(today["avg_usage_in_mb"])*1024*1024));
+    $("#active14d").html(today["count_14_day_actives"]);
+    /*
+    for (var row in data){
+        var date = data[row]["date"];
+        date = date.substring(6)+"."+date.substring(4,6)+"."+date.substring(0,4);
+        avg_dates.push(date);
+        var users = data[row]["num_accounts"];
+        var usage = bytesToSize(data[row]["usage_in_bytes"]);
+        var avg = bytesToSize(parseInt(data[row]["avg_usage_in_mb"])*1024*1024);
+        avg_values.push(parseInt(data[row]["avg_usage_in_mb"]));
+
+        $("#gapptable tbody").append("<tr></tr><td>"+date+"</td><td>"+users+"</td><td>"+usage+"</td><td>"+avg+"</td></tr>");
+    } */
+
+    //TODO: Draw graphs
+}
+
+$(document).ready(function () {
+    $("#update_data").pagerefresh({"short_timeout": 1*60*60, "long_timeout": 1*60*60, "filewatch": "gapps.json"});
+
+});
---
+++ pages/gapps.php
@@ -0,0 +1,39 @@
+<script src="/js/combined.raphael.min.js"></script>
+<script type="text/javascript"><?php readfile("js/gapps.min.js");?></script>
+
+
+<h1>Google Apps <span id="update_data"></span></h1>
+
+<div class="row">
+<div class="span2">
+<h2><small>Accounts</small></h2>
+<h2 id="users"><span class="loading-inline-gif"></span></h2>
+</div>
+
+<div class="span2">
+<h2><small>Usage</small></h2>
+<h2 id="usage"><span class="loading-inline-gif"></span></h2>
+</div>
+
+<div class="span2">
+<h2><small>Average</small></h2>
+<h2 id="average"><span class="loading-inline-gif"></span></h2>
+</div>
+
+</div>
+
+<div class="row">
+<div class="span6">
+<h2><small>Active accounts in the past 14 days</small></h2>
+<h2 id="active14d"><span class="loading-inline-gif"></span></h2>
+</div>
+
+</div>
+
+<div class="row" style="padding-top:1em">
+<div class="span6">
+<div id="holder"></div>
+
+</div>
+</div>
+
